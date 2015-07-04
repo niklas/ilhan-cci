61a3e8bef84b94c02d671ff2459ad63772e49308
Niklas Hofer <niklas+dev@lanpartei.de>
2015-07-04 23:20:49 +0200

    plot.. something

---
+++ lib/utils.rb
@@ -0,0 +1,24 @@
+require 'gnuplot'
+class Utils
+  def self.plot(values, options={})
+    Gnuplot.open do |gp|
+      Gnuplot::Plot.new( gp ) do |pl|
+
+        pl.title( options.fetch(:title) { 'ein paar Werte' } )
+        pl.xlabel "i"
+        pl.ylabel "c"
+
+        pl.terminal "png"
+        pl.output( options.fetch(:output) { 'plot.png' } )
+
+        x = (0..values.length).collect { |v| v.to_f }
+
+        pl.data << Gnuplot::DataSet.new( [x, values] ) do |ds|
+          ds.with = "points"
+          ds.notitle
+        end
+      end
+
+    end
+  end
+end
