5e166c7627aea9795dc35ca171cee925ec6429bf
Niklas Hofer <niklas+dev@lanpartei.de>
2015-07-06 16:43:45 +0200

    spec using example values from JS inspector

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+
+end
+require_relative '../lib/crunch'
